cc56869b1b7c45a5412f5a97f38706312bbb8936
Simar Vashisht <simarv07@gmail.com>
2024-05-02 14:22:52 -0700

    Added route after user logs in

--- src/app/api/login/addToSupabase/route.js
+++ src/app/api/login/addToSupabase/route.js
@@ -1,5 +1,44 @@
 import { NextResponse } from "next/server";
+import Database from "../../../database";
+import { currentUser } from "@clerk/nextjs/server";
 
-export function GET() {
-  return NextResponse.json({ message: "Hello from ClubHub!" });
+function checkIfUserExists(clerkId) {
+    return Database.from("user").select("*").eq("clerkId", clerkId);
+}
+
+// POST /api/campus
+export async function POST(req) {
+  try {
+    const user = await currentUser();
+    if (!user) {
+      return NextResponse.error(new Error("User not found"));
+    }
+
+    const firstName = user.firstName;
+    const lastName = user.lastName;
+    const email = user.email;
+    const clerkId = user.id;
+
+    if (checkIfUserExists(clerkId)) {
+        return NextResponse.json(
+            { message: "User already exists" }
+        )
+    }
+
+    const { data, error } = await Database.from("user")
+      .insert([{ firstName: firstName, 
+        lastName: lastName, 
+        email: email,
+        clerkId: clerkId }])
+      .select();
+
+    if (error) {
+      throw new Error(error);
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error(`Error creating campus: ${error}`);
+    return NextResponse.error(error);
+  }
 }
