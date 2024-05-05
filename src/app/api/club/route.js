1248a9c471bec22f2b6c10996207d9c6a08c2a47
jjrim <jjyyrim@gmail.com>
2024-05-04 20:29:50 -0700

    add post and get routes for club data

---
+++ src/app/api/club/route.js
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import Database from "../../database";
+
+// GET /api/club
+export async function GET() {
+    try {
+      let { data: club, error } = await Database.from("club").select("*");
+      if (error) {
+        throw new Error(error);
+      }
+      return NextResponse.json(club);
+    } catch (error) {
+      console.error(`Error fetching club: ${error}`);
+      return NextResponse.error(error);
+    }
+  }
+
+// POST /api/club
+export async function POST(req) {
+    try {
+      const { name, description, campus_id, abbreviation, website } = await req.json();
+  
+      if (!name || !campus_id || !description) {
+        return NextResponse.json(
+          { error: "Missing required properties" },
+          { status: 400 }
+        );
+      }
+  
+      const { data, error } = await Database.from("club")
+        .insert([{ name: name, campus_id:campus_id, description: description, abbreviation: abbreviation, website:website }])
+        .select();
+  
+      if (error) {
+        throw new Error(error);
+      }
+      return NextResponse.json(data);
+    } catch (error) {
+      console.error(`Error creating club: ${error}`);
+      return NextResponse.error(error);
+    }
+  }
