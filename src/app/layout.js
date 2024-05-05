33c3d922e78cb6655df09265bf162f04b95f9042
ferrelanthoni <ferrelanthoni@gmail.com>
2024-05-05 11:37:10 -0700

    render style components

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
-// import "./globals.css";
+import StyledComponentsRegistry from "./registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-      </body>
+      <StyledComponentsRegistry>
+        <body className={inter.className}>
+          <Navbar />
+          {children}
+        </body>
+      </StyledComponentsRegistry>
     </html>
   );
 }
