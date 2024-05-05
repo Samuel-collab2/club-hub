f9828ee76d5e67316beac9f8cdcfccb1ba4aff41
Simar Vashisht <simarv07@gmail.com>
2024-05-05 02:28:14 -0700

    Finished delete and get events route

---
+++ src/app/api/query/events/route.js
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import Database from "../../../database";
+
+// POST /api/query/events
+export async function POST(req) {
+    try {
+      const { clubId, includePastEvents } = await req.json();
+  
+      let query = Database.from("event").select("*");
+
+      if (clubId) {
+        query = query.eq("clubId", clubId);
+      }
+  
+      if (includePastEvents === false) {
+        const now = new Date().toISOString();
+        query = query.gt("dateTime", now);
+      }
+  
+      const { data, error } = await query;
+  
+      if (error) {
+        throw new Error(error);
+      }
+
+      if (data[0] === undefined){
+        return NextResponse.json({message: "No events found"});
+      }
+
+      return NextResponse.json(data);
+    } catch (error) {
+      console.error(`Error querying events: ${error}`);
+      return NextResponse.error(error);
+    }
+  }
