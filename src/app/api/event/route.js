addf7a86cc1ff3152a5fee9cbcdee58613775540
ferrelanthoni <ferrelanthoni@gmail.com>
2024-05-13 21:26:38 -0700

    upload image to a bucket

--- src/app/api/event/route.js
+++ src/app/api/event/route.js
@@ -1,44 +1,71 @@
 import Database from "../../database";
 import { NextResponse } from "next/server";
+import { decode } from "base64-arraybuffer";
 
 // POST /api/event
 export async function POST(req) {
-    try {
-        // Note: dateTime string has to be in form that is readable to supabase [[supabase expects a time stamp with time zone]]
-        // Ex: "05 October 2011 14:48 UTC"
-        // Ex2: "11 January 1999" // doesn't need clock time or timezone, just Date is mandatory
-        const {clubId, name, dateTime, description, location, isVirtual, maxCapacity, banner} = await req.json()
+  try {
+    const {
+      clubId,
+      name,
+      dateTime,
+      description,
+      location,
+      isVirtual,
+      maxCapacity,
+      banner,
+    } = await req.json();
 
-        if (!clubId || !dateTime || !location) {
-          return NextResponse.json(
-            { error: "Missing required properties" },
-            { status: 400 }
-          );
-        }
-
-        const { data, error } = await Database.from("event")
-          .insert([
-            {
-              clubId: clubId,
-              name: name,
-              dateTime: dateTime,
-              description: description,
-              location: location,
-              isVirtual: isVirtual ? isVirtual : false,
-              maxCapacity: maxCapacity,
-              banner: banner,
-            },
-          ])
-          .select();
-
-        if (error) {
-          throw new Error(error);
-        } else {
-        return NextResponse.json(data[0]);
-        }
-        
-    } catch (error) {
-        console.error(`Error found during event creation: ${error}`);
-        return NextResponse.error(error);
+    if (!clubId || !dateTime || !location) {
+      return NextResponse.json(
+        { error: "Missing required properties" },
+        { status: 400 }
+      );
     }
+
+    const eventImage = banner.split("base64,")[1];
+    let bannerUrl;
+
+    const { data: bannerImageData, error: uploadBannerError } =
+      await Database.storage
+        .from("eventImage")
+        .upload(`${clubId}-${name}-${dateTime}`, decode(eventImage), {
+          contentType: "image/png",
+          upsert: true,
+        });
+
+    if (uploadBannerError) {
+      throw new Error(uploadBannerError);
+    } else {
+      const imagePath = bannerImageData.path;
+      const { data: url } = Database.storage
+        .from("eventImage")
+        .getPublicUrl(imagePath);
+      bannerUrl = url.publicUrl;
+    }
+
+    const { data, error } = await Database.from("event")
+      .insert([
+        {
+          clubId: clubId,
+          name: name,
+          dateTime: dateTime,
+          description: description,
+          location: location,
+          isVirtual: isVirtual ? isVirtual : false,
+          maxCapacity: maxCapacity,
+          banner: bannerUrl,
+        },
+      ])
+      .select();
+
+    if (error) {
+      throw new Error(error);
+    } else {
+      return NextResponse.json(data[0]);
+    }
+  } catch (error) {
+    console.error(`Error found during event creation: ${error}`);
+    return NextResponse.error(error);
   }
+}
