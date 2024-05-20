5de4be3a759ac69bc9a3e2a33003f5cecde8d40d
Samuel Cheon <samuelcalling@gmail.com>
2024-05-20 12:33:59 -0700

    completed GET event participants count api

--- src/app/api/participant/[id]/count/route.js
+++ src/app/api/participant/[id]/count/route.js
@@ -1,41 +1,32 @@
 import {NextResponse} from "next/server";
 import Database from "../../../../database";
 
-// POST /api/participants/[id]/count
-export async function POST(req) {
-    try {
-        const {participantId} = await req.json()
+// GET /api/participant/[id]/count
+export async function GET(req) {
 
-        if (!participantId) {
+    try {
+        const eventId = req.url.slice(req.url.lastIndexOf("participant/") + 12, req.url.lastIndexOf("/count"));
+
+        if (!eventId) {
             return NextResponse.json(
               { error: "Missing required properties" },
               { status: 400 }
             );
           }
 
-        const { data, error } = await Database.from("eventparticipants")
+        const { data, error } = await Database.from("event")
             .select()
-            .eq('id', participantId)
+            .eq('id', eventId)
 
         if (error) {
             throw new Error(error);
         } else if (Object.keys(data).length === 0) {
-            throw new Error("passed in participant id not found in database");
-        }
-
-        const eventData = await Database.from("event")
-            .select()
-            .eq('id', data[0].eventId)
-
-        if (eventData.error) {
-            throw new Error(eventData.error);
-        } else if (Object.keys(eventData.data).length === 0) {
-            throw new Error("event id associated with participant id not found in database");
+            throw new Error("passed in event id not found in database");
         }
 
         return NextResponse.json({
-            "eventId": eventData.data[0].id, 
-            "participantsCount": eventData.data[0].participantsCount
+            "eventId": eventId, 
+            "participantsCount": data[0].participantsCount
         });
 
     } catch (error) {
