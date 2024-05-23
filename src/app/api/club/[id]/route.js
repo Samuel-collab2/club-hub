4914714e4f44da32d349f9befa6bd81eec6e5419
ferrelanthoni <ferrelanthoni@gmail.com>
2024-05-23 15:36:38 -0700

    subscribe unsubscribe club

--- src/app/api/club/[id]/route.js
+++ src/app/api/club/[id]/route.js
@@ -1,23 +1,71 @@
 import { NextResponse, NextRequest } from "next/server";
 import Database from "../../../database";
+import { currentUser } from "@clerk/nextjs/server";
+
+// Get currently logged in user id
+const getCurrentUserId = async () => {
+  const user = await currentUser();
+  const userId = await Database.from("user")
+    .select("id")
+    .eq("clerkId", user.id)
+    .single()
+    .then((user) => user.data.id);
+  return userId;
+};
 
 // GET /api/club/[id]
 export async function GET(req) {
-    try {
-        const clubId = req.url.slice(req.url.lastIndexOf("/") + 1);
-        let { data: club, error } = await Database.from("club").select().eq("id", clubId);
-        if (!req) {
-            return NextResponse.json({ error: "Invalid request." }, { status: 400 }); 
-        }
-        else if (!clubId) {
-            return NextResponse.json({ error: "Club id is required." }, { status: 404 }); 
-        }
-      return NextResponse.json(club[0]);
-    } catch (error) {
-      console.error(`Error getting club: ${error} for club id: ${clubId}`);
-      return NextResponse.error(error);
+  const isQueryingSubscribed = req.nextUrl.searchParams.get("subscribed");
+
+  try {
+    const clubId = req.nextUrl.pathname.slice(
+      req.nextUrl.pathname.lastIndexOf("/") + 1
+    );
+    console.log("clubId: ", clubId);
+    let { data: club, error } = await Database.from("club")
+      .select()
+      .eq("id", clubId);
+    if (!req) {
+      return NextResponse.json({ error: "Invalid request." }, { status: 400 });
+    } else if (!clubId) {
+      return NextResponse.json(
+        { error: "Club id is required." },
+        { status: 404 }
+      );
     }
+
+    if (isQueryingSubscribed && isQueryingSubscribed === "true") {
+      const userId = await getCurrentUserId();
+      if (!userId) {
+        return NextResponse.error(new Error("User not found"), { status: 404 });
+      }
+
+      const { data: subscribed, error: subError } = await Database.from(
+        "clubSubscriber"
+      )
+        .select()
+        .eq("userId", userId)
+        .eq("clubId", clubId);
+
+      if (subError) {
+        throw new Error(subError);
+      }
+      club[0].isSubscribed = subscribed.length > 0;
+    }
+
+    const { data: subscriberCount, error: subCountError } = await Database.from(
+      "clubSubscriber"
+    )
+      .select("clubId")
+      .eq("clubId", clubId);
+
+    club[0].subscriberCount = subscriberCount.length;
+    return NextResponse.json(club[0]);
+  } catch (error) {
+    console.error(`Error getting club: ${error} for club id: ${clubId}`);
+    return NextResponse.error(error);
   }
+}
 
 
 // PATCH /api/club/[id]
